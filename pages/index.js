d51228c297a5778740ad833b06467288ee529fa7
Somesh Singh <someshsingh298@gmail.com>
2022-05-15 13:52:02 +0530

    connected mongo db

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import NavBar from "../components/NavBar";
+import clientPromise from "../mongodb-config";
+import { useEffect } from "react";
 
 function Home() {
+  useEffect(async () => {
+    await clientPromise;
+    console.log("Connected");
+  });
   return (
     <>
       <NavBar />
