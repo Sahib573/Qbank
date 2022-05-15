e421c3780c405bf20f93abd7c8f2603c5864721e
Somesh Singh <someshsingh298@gmail.com>
2022-05-15 17:32:09 +0530

    question upload complete

--- components/Questions/UploadQuestions.js
+++ components/Questions/UploadQuestions.js
@@ -1,6 +1,54 @@
-import React, { useStates } from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
 
 function UploadQuestions() {
+  const [questionHeading, setQuestionHeading] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
+  const [option3, setOption3] = useState("");
+  const [option4, setOption4] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [subject, setSubject] = useState("english");
+  const [explanation, setExplanation] = useState("");
+  const { currentUser } = useAuth();
+  console.log(currentUser._delegate.displayName);
+  console.log(currentUser._delegate.email);
+  const addQuestionHandler = async () => {
+    const response = await axios.post("/api/push_question", {
+      title: questionHeading,
+      option1: option1,
+      option2: option2,
+      option3: option3,
+      option4: option4,
+      answer: answer,
+      author: currentUser._delegate.displayName,
+      author_email: currentUser._delegate.email,
+      explanation: explanation,
+      subject_name: subject,
+    });
+    console.log(response.data);
+  };
+  const subjectChangeHandler = (e) => {
+    switch (e) {
+      case "English":
+        setSubject("english");
+        break;
+      case "Hindi":
+        setSubject("hindi");
+        break;
+      case "Maths":
+        setSubject("maths");
+        break;
+      case "Science":
+        setSubject("science");
+        break;
+      case "Social Science":
+        setSubject("social_science");
+        break;
+    }
+    console.log(subject);
+  };
   return (
     <div>
       <div>Question Title</div>
@@ -8,7 +56,18 @@ function UploadQuestions() {
         <input
           type="text"
           className="border-2 border-purple-300 rounded-lg h-10 w-2/3"
+          onChange={(e) => setQuestionHeading(e.target.value)}
         />
+      </div>
+      <div>Subject Name </div>
+      <div>
+        <select onClick={(e) => subjectChangeHandler(e.target.value)}>
+          <option>English</option>
+          <option>Maths</option>
+          <option>Science</option>
+          <option>Social Science</option>
+          <option>Hindi</option>
+        </select>
       </div>
       <div>
         <div>Option - 1 </div>
@@ -16,6 +75,54 @@ function UploadQuestions() {
           <input
             type="text"
             className="border-2 border-purple-300 rounded-lg h-10 w-2/3"
+            onChange={(e) => setOption1(e.target.value)}
+          />
+        </div>
+        <div>Option - 2 </div>
+        <div>
+          <input
+            type="text"
+            className="border-2 border-purple-300 rounded-lg h-10 w-2/3"
+            onChange={(e) => setOption2(e.target.value)}
+          />
+        </div>
+        <div>Option - 3 </div>
+        <div>
+          <input
+            type="text"
+            className="border-2 border-purple-300 rounded-lg h-10 w-2/3"
+            onChange={(e) => setOption3(e.target.value)}
+          />
+        </div>
+        <div>Option - 4 </div>
+        <div>
+          <input
+            type="text"
+            className="border-2 border-purple-300 rounded-lg h-10 w-2/3"
+            onChange={(e) => setOption4(e.target.value)}
+          />
+        </div>
+        <div>Answer </div>
+        <div>
+          <input
+            type="text"
+            className="border-2 border-purple-300 rounded-lg h-10 w-2/3"
+            onChange={(e) => setAnswer(e.target.value)}
+          />
+        </div>
+        <div>Explanation </div>
+        <div>
+          <input
+            type="text"
+            className="border-2 border-purple-300 rounded-lg h-10 w-2/3"
+            onChange={(e) => setExplanation(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="submit"
+            className="border-2 border-purple-300 rounded-lg mt-10 h-10 w-2/3"
+            onClick={addQuestionHandler}
           />
         </div>
       </div>
---
+++ pages/api/push_question.js
@@ -0,0 +1,43 @@
+import clientPromise from "../../mongodb-config";
+
+export default async function Users(req, res) {
+  if (req.method === "POST") {
+    const client = await clientPromise;
+    const db = client.db("subject_questions");
+    const subject_name = req.body.subject_name;
+    const id_ = (
+      (await db.collection("question_details").count()) + 1
+    ).toString();
+    const questionArrayDetails = await db
+      .collection(subject_name)
+      .find({})
+      .toArray();
+    console.log(questionArrayDetails[0]);
+    const newQuestionArray = questionArrayDetails[0].questionId;
+    newQuestionArray.push(id_);
+    console.log(newQuestionArray);
+    await db
+      .collection(subject_name)
+      .updateOne(
+        { _id: questionArrayDetails[0]._id },
+        { $set: { questionId: newQuestionArray } }
+      );
+    await db.collection("question_details").insertOne({
+      title: req.body.title,
+      option1: req.body.option1,
+      option2: req.body.option2,
+      option3: req.body.option3,
+      option4: req.body.option4,
+      answer: req.body.answer,
+      author: req.body.author,
+      author_email: req.body.author_email,
+      explanation: req.body.explanation,
+      question_id: id_,
+    });
+
+    console.log("question Posted");
+    res.json({ questionPosted: req.body });
+  } else {
+    res.json({ message: "This request is not allowed", user: false });
+  }
+}
