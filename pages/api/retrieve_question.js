8d726f8484e75ed7c8e4d232ed4e1d5851fbac95
Sahib573 <gurdeepsingh74108@gmail.com>
2022-05-15 19:12:35 +0530

    retrive bug fixing

--- pages/api/retrieve_question.js
+++ pages/api/retrieve_question.js
@@ -6,22 +6,30 @@ export default async function Users(req, res) {
     const db = client.db("subject_questions");
     const subj = req.body.subject_name;
     const arr = [];
-    await db
+    // await db
+    //   .collection(subj)
+    //   .find({})
+    //   .forEach(async (ind) => {
+    //     ind.questionId.forEach(async (question) => {
+    //       const question_retrieved = await db
+    //         .collection("question_details")
+    //         .find({ question_id: question })
+    //         .toArray();
+    //       arr.push(question_retrieved);
+    //     });
+    //   });
+    const data = await db
       .collection(subj)
       .find({})
       .forEach(async (ind) => {
         ind.questionId.forEach(async (question) => {
-          const question_retrieved = await db
-            .collection("question_details")
-            .find({ question_id: question })
-            .toArray();
-          arr.push(question_retrieved);
+          console.log(question);
         });
       });
-    console.log(arr.length);
+    console.log(data);
     // const data = await db.collection(subj).find({}).toArray();
-    // if (arr) {
-    //   const response = JSON.parse(JSON.stringify(arr));
+    // if (data) {
+    //   const response = JSON.parse(JSON.stringify(data));
     //   res.json(response);
     // }
     res.json({ message: "No questions available", user: false });
